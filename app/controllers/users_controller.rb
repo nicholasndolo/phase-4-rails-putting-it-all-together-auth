2d89736d8e6d20eac289e79af782ada083f10b66
Nicholas <nicholas.nmalinda@gmail.com>
2022-10-07 15:23:08 +0300

    Lab ompleted

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+    # before_action :authenticate
+# rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_respons
+    wrap_parameters format: []
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: {errors: user.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+    def show
+        user = User.find_by(id: session[:user_id])
+        if user
+            render json: user, status: :created
+        else
+            render json: {error: "Not authorized"}, status: :unauthorized
+        end
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation, :image_url, :bio)
+    end
+
+    # def render_unprocessable_entity_response(invalid)
+    #     render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+    # end
+end
